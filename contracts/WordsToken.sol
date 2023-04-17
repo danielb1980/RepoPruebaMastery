2d7ab142e470cbeda2ea4127ee0e5a3bd854a700
danielb1980 <daniel@danielbogado.com.ar>
2023-04-17 13:50:27 -0300

    cambios nombre Contrato

--- contracts/WordsToken.sol
+++ contracts/WordsToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract WordsToken is ERC20 {
     constructor() ERC20("Words", "WDS") {
         _mint(msg.sender,100000000000000000000000000);
     }
